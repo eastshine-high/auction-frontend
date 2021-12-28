c021f2d3216032110dfda367a8affaf3de2f7c1c
eastshine-high <eastshine.high@gmail.com>
2021-12-28 20:47:26 +0900

    apply React-router
    1. apply react router
    2. add base pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { Routes, Route } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+import PostPage from './pages/PostPage';
+import PostListPage from './pages/PostListPage';
 
-function App() {
+//<Route element={<PostListPage />} path={['/@:username', '/']} exact />
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route element={<PostListPage />} path="/" exact />
+        <Route element={<LoginPage />} path="/login" />
+        <Route element={<RegisterPage />} path="/register" />
+        <Route element={<WritePage />} path="/write" />
+        <Route element={<PostPage />} path="/@:username/:postId" />
+      </Routes>
+    </>
   );
-}
-
+};
 export default App;
