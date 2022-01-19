aa28be1b887944b109fb42cc5450cec5fcee1278
eastshine-high <eastshine.high@gmail.com>
2022-01-19 11:16:55 +0900

    manage form status with redux

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import palette from '../../lib/styles/palette';
 import Button from '../common/Button';
 
@@ -9,11 +9,11 @@ import Button from '../common/Button';
  */
 
 const AuthFormBlock = styled.div`
-    h3{
-        margin: 0;
-        color: ${palette.gray[8]};
-        margin-bottom: 1rem
-    }
+  h3 {
+    margin: 0;
+    color: ${palette.gray[8]};
+    margin-bottom: 1rem;
+  }
 `;
 
 /**
@@ -36,64 +36,74 @@ const StyledInput = styled.input`
 `;
 
 const ButtonWithMarginTop = styled(Button)`
-    margin-top: 1rem;
+  margin-top: 1rem;
 `;
 
 /**
  * 폼 하단에 로그인 혹은 회원가입 링크를 보여 줌
  */
 const Footer = styled.div`
-    margin-top: 2rem;
-    text-align: right;
-    a {
-        color: ${palette.gray[6]};
-        text-decoration: underline;
-        &:hover{
-            color: ${palette.gray[9]};
-        }
+  margin-top: 2rem;
+  text-align: right;
+  a {
+    color: ${palette.gray[6]};
+    text-decoration: underline;
+    &:hover {
+      color: ${palette.gray[9]};
     }
+  }
 `;
 
 const textMap = {
-    login: '로그인',
-    register: '회원가입',
+  login: '로그인',
+  register: '회원가입',
 };
 
-const AuthForm = ({ type }) => {
-    const text = textMap[type];
-    return (
-        <AuthFormBlock>
-            <h3>{text}</h3>
-            <form>
-                <StyledInput autoComplete='username' name='username' placeholder='아이디' />
-                <StyledInput
-                    autoComplete='new-password'
-                    name='password'
-                    placeholder='비밀번호'
-                    type='password'
-                />
-                {type === 'register' && (
-                    <StyledInput
-                        autoComplete='new-password'
-                        name='passwordConfirm'
-                        placeholder='비밀번호 확인'
-                        type='password'
-                    />
-                )}
-                <ButtonWithMarginTop cyan fullWidth>
-                {/*cyan={true} fullWidth={true} */}
-                    {text}
-                </ButtonWithMarginTop>
-            </form>
-            <Footer>
-                {type === 'login' ?(
-                    <Link to="/register">회원가입</Link>
-                ) : (
-                    <Link to="/login">로그인</Link>
-                )}
-            </Footer>
-        </AuthFormBlock>
-    );
+const AuthForm = ({ type, form, onChange, onSubmit }) => {
+  const text = textMap[type];
+  return (
+    <AuthFormBlock>
+      <h3>{text}</h3>
+      <form onSubmit={onSubmit}>
+        <StyledInput
+          autoComplete="username"
+          name="username"
+          placeholder="아이디"
+          onChange={onChange}
+          value={form.username}
+        />
+        <StyledInput
+          autoComplete="new-password"
+          name="password"
+          placeholder="비밀번호"
+          type="password"
+          onChange={onChange}
+          value={form.password}
+        />
+        {type === 'register' && (
+          <StyledInput
+            autoComplete="new-password"
+            name="passwordConfirm"
+            placeholder="비밀번호 확인"
+            type="password"
+            onChange={onChange}
+            value={form.passwordConfirm}
+          />
+        )}
+        <ButtonWithMarginTop cyan fullWidth style={{ marginTop: '1rem' }}>
+          {/*cyan={true} fullWidth={true} */}
+          {text}
+        </ButtonWithMarginTop>
+      </form>
+      <Footer>
+        {type === 'login' ? (
+          <Link to="/register">회원가입</Link>
+        ) : (
+          <Link to="/login">로그인</Link>
+        )}
+      </Footer>
+    </AuthFormBlock>
+  );
 };
 
-export default AuthForm
+export default AuthForm;
